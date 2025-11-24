00023861bbda04583060cd5f3f6a52c7ec934943
chenpingfeng <717903589@qq.com>
2025-11-24 21:45:11 +0800

    我的二次修改

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,23 @@
-<h1>Hello Index!</h1>
+@extends('layouts.app')
+
+@section('title', 'Task List')
+
+<!-- <h1>Hello Index!</h1> -->
+
+@section('content')
 <div>
     @if (count($mytasks))
         @foreach ($mytasks as $task)
             <div>
                 <a href="{{ route('task.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
             </div>
-
         @endforeach
     @else
         <h2>No Tasks Yet</h2>
     @endif
-    
 </div>
+
+@endsection
 
 <!-- <div>
     @forelse ($mytasks as $task)
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,1 +1,17 @@
-<h1>{{ $task->title }}</h1>
+@extends('layouts.app')
+
+@section('title',$task->title)
+
+
+
+@section('content')
+    <h2>{{ $task->title }}</h2>
+    <p>{{ $task->description }}</p>
+
+    @if($task->long_description)
+        <p>{{ $task->long_description }}</p>
+    @endif
+
+    <p>Created at: {{ $task->created_at }}</p>
+    <p>Updated at: {{ $task->updated_at }}</p>
+@endsection
